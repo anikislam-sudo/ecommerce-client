fe1d1138ff773b0b0ac3431dd6e68bca724334c3
unknown <islammoshiu@gmail.com>
2023-01-21 12:39:00 +0600

    banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,17 @@
 import React from 'react';
-
+import banner from "../../../Assets/banner-removebg-preview.png";
 const Banner = () => {
     return (
-        <div className=''>
-            
+        <div className="hero my-3 bg-yellow-200">
+        <div className="hero-content my-6 flex-col lg:flex-row-reverse">
+          <img src={banner} alt='' className=" w-100% " />
+          <div>
+            <h1 className="text-5xl font-bold">END OF <br></br>SPRING COLLECTION!</h1>
+            <p className="py-6 text-lg ">Save up to 25% + free shipping </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from "../../../Assets/Easy Cart.png";
+import logo from "../../../Assets/Easy_Cart-removebg-preview.png";
 import "./Navbar.css";
 const Navbar = () => {
     const menuItems =
@@ -12,7 +12,7 @@ const Navbar = () => {
 
     </React.Fragment>
     return (
-        <div className="navbar bg-orange-500">
+        <div className="navbar  bg-orange-500 flex justify-between">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -24,8 +24,8 @@ const Navbar = () => {
           </div>
           <Link to="/" className="btn btn-ghost normal-case text-xl"><img className='logo' src={logo} alt="" srcset="" /></Link>
         </div>
-        <div className="navbar-end text-xl hidden lg:flex">
-          <ul className="menu menu-horizontal px-5">
+        <div className="navbar-center text-lg text-white hidden lg:flex">
+          <ul className="menu menu-horizontal px-2">
           {menuItems}
           </ul>
         </div>
